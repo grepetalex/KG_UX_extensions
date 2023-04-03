81d7e1a98ee8845f027c354a9ad34dce64f64f0b
Puncher <puncher@gmail.com>
2023-04-03 19:51:18 +0300

    Minor fixes

--- KG_Chat_Users_Tracker.js
+++ KG_Chat_Users_Tracker.js
@@ -431,13 +431,13 @@
 
   // SOUND GRAPHICAL SWITCHER
   // Button SVG icons muted and unmuted representation
-  const iconSoundMuted = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-  stroke-linecap="round" stroke-linejoin="round" class="feather feather-volume-1">
+  const iconSoundMuted = `<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="hsl(355, 80%, 65%)" stroke-width="1.4"
+      stroke-linecap="round" stroke-linejoin="round" class="feather feather-volume-x">
       <polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon>
-      <path d="M15.54 8.46a5 5 0 0 1 0 7.07"></path>
+      <line x1="23" y1="9" x2="17" y2="15"></line>
+      <line x1="17" y1="9" x2="23" y2="15"></line>
       </svg>`;
-
-  const iconSoundUnmuted = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+  const iconSoundUnmuted = `<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="hsl(80, 80%, 40%)" stroke-width="1.4"
   stroke-linecap="round" stroke-linejoin="round" class="feather feather-volume-2">
       <polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon>
       <path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"></path>
@@ -445,7 +445,7 @@
   // New message sound notification graphical switcher as a button
   const chatButtonsPanel = document.querySelector('.chat .messages table td:nth-child(3)');
   // Avoid panel squeezing
-  chatButtonsPanel.style.minWidth = '75px';
+  chatButtonsPanel.style.minWidth = '105px';
   const soundSwitcher = document.createElement('div');
 
   // Check if the user has previously muted the sound
@@ -467,7 +467,7 @@
   const soundIcon = document.createElement('span');
   soundIcon.classList.add('sound-icon');
   // iconSoundMuted > 🔉 iconSoundUnmuted > 🔊
-  soundIcon.textContent = newMessageIsMuted ? iconSoundMuted : iconSoundUnmuted;
+  soundIcon.innerHTML = newMessageIsMuted ? iconSoundMuted : iconSoundUnmuted;
   // Append button and icon inside the button
   soundSwitcher.appendChild(soundIcon);
   chatButtonsPanel.appendChild(soundSwitcher);
@@ -475,9 +475,9 @@
   function updateSoundIcon() {
     const soundIcon = soundSwitcher.querySelector('.sound-icon');
     if (soundSwitcher.id === 'muted') {
-      soundIcon.textContent = iconSoundMuted; // 🔉
+      soundIcon.innerHTML = iconSoundMuted; // 🔉
     } else {
-      soundIcon.textContent = iconSoundUnmuted; // 🔊
+      soundIcon.innerHTML = iconSoundUnmuted; // 🔊
     }
   }
 

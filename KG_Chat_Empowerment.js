77466c6b6af390a6676b0de63fd5a52ec9cd79be
Puncher <puncher@gmail.com>
2023-04-20 02:34:10 +0300

    add opacity 0.3 to beep icon path

--- KG_Chat_Empowerment.js
+++ KG_Chat_Empowerment.js
@@ -1164,11 +1164,13 @@
       </svg>`;
   const iconSoundBeep = `<svg xmlns="http://www.w3.org/2000/svg" width="${iconSize}" height="${iconSize}" viewBox="0 0 24 24" fill="none" stroke="${iconBeepStroke}" stroke-width="${iconStrokeWidth}" stroke-linecap="round" stroke-linejoin="round">
       <polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon>
+      <path d="M19.07 4.93a10 10 0 0 1 0 14.14" opacity="0.3"></path>
       <path d="M15.54 8.46a5 5 0 0 1 0 7.07"></path>
       </svg>`;
   const iconSoundVoice = `<svg xmlns="http://www.w3.org/2000/svg" width="${iconSize}" height="${iconSize}" viewBox="0 0 24 24" fill="none" stroke="${iconVoiceStroke}" stroke-width="${iconStrokeWidth}" stroke-linecap="round" stroke-linejoin="round">
       <polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon>
-      <path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"></path>
+      <path d="M19.07 4.93a10 10 0 0 1 0 14.14"></path>
+      <path d="M15.54 8.46a5 5 0 0 1 0 7.07"></path>
       </svg>`;
 
   // Icons for message mode button

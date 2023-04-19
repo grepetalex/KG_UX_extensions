de7b3629ec4525e372a99baa1ba6d5fda2d14b72
Puncher <puncher@gmail.com>
2023-04-20 02:37:35 +0300

    added different colors to message modes

--- KG_Chat_Empowerment.js
+++ KG_Chat_Empowerment.js
@@ -1175,13 +1175,13 @@
 
   // Icons for message mode button
   // Button SVG icons "every", "mention" representation
-  const iconModeEvery = `<svg xmlns="http://www.w3.org/2000/svg" width="${iconSize}" height="${iconSize}" viewBox="0 0 24 24" fill="none" stroke="#b2b2b2" stroke-width="${iconStrokeWidth}" stroke-linecap="round" stroke-linejoin="round">
+  const iconModeEvery = `<svg xmlns="http://www.w3.org/2000/svg" width="${iconSize}" height="${iconSize}" viewBox="0 0 24 24" fill="none" stroke="hsl(100, 50%, 50%)" stroke-width="${iconStrokeWidth}" stroke-linecap="round" stroke-linejoin="round">
       <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
       <circle cx="9" cy="7" r="4"></circle>
       <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
       <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
       </svg>`;
-  const iconModeMention = `<svg xmlns="http://www.w3.org/2000/svg" width="${iconSize}" height="${iconSize}" viewBox="0 0 24 24" fill="none" stroke="#b2b2b2" stroke-width="${iconStrokeWidth}" stroke-linecap="round" stroke-linejoin="round">
+  const iconModeMention = `<svg xmlns="http://www.w3.org/2000/svg" width="${iconSize}" height="${iconSize}" viewBox="0 0 24 24" fill="none" stroke="hsl(180, 60%, 50%)" stroke-width="${iconStrokeWidth}" stroke-linecap="round" stroke-linejoin="round">
       <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
       <circle cx="12" cy="7" r="4"></circle>
       </svg>`;

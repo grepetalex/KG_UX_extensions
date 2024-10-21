2d2677be49709704239842e6d44c77882b5efc8b
Puncher <puncher@gmail.com>
2024-10-21 03:51:31 +0300

    added update and download headers for tampermonkey extension.

--- KG_Chat_Empowerment.js
+++ KG_Chat_Empowerment.js
@@ -6,6 +6,8 @@
 // @author       Patcher
 // @match        *://klavogonki.ru/g*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=klavogonki.ru
+// @updateURL    https://raw.githubusercontent.com/VimiummuimiV/KG_Goddies/refs/heads/master/KG_Chat_Empowerment.js
+// @downloadURL  https://raw.githubusercontent.com/VimiummuimiV/KG_Goddies/refs/heads/master/KG_Chat_Empowerment.js
 // @grant        none
 // ==/UserScript==
 

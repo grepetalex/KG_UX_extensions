7b1af37b7ba3d8bbe58a54bc303d1abb7fa209ea
Puncher <puncher@gmail.com>
2024-10-29 04:39:55 +0200

    Small fix.

--- KG_Chat_Empowerment.js
+++ KG_Chat_Empowerment.js
@@ -3629,6 +3629,40 @@
 
   const popupMessageIconSize = 16;
 
+  // SVG markup for a clock icon
+  const clockSVG = `
+  <svg xmlns="http://www.w3.org/2000/svg"
+       width="${popupMessageIconSize - 2}"
+       height="${popupMessageIconSize - 2}"
+       viewBox="0 0 24 24"
+       fill="none"
+       stroke="currentColor"
+       stroke-width="2"
+       stroke-linecap="round"
+       stroke-linejoin="round"
+       class="feather feather-clock">
+    <circle cx="12" cy="12" r="10"></circle>
+    <polyline points="12 6 12 12 16 14"></polyline>
+  </svg>
+`;
+
+  // SVG markup for a user icon
+  const userSVG = `
+  <svg xmlns="http://www.w3.org/2000/svg"
+       width="${popupMessageIconSize - 2}"
+       height="${popupMessageIconSize - 2}"
+       viewBox="0 0 24 24"
+       fill="none"
+       stroke="currentColor"
+       stroke-width="2"
+       stroke-linecap="round"
+       stroke-linejoin="round"
+       class="feather feather-user">
+    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+    <circle cx="12" cy="7" r="4"></circle>
+  </svg>
+`;
+
   const popupChatMessageStyles = document.createElement('style');
   popupChatMessageStyles.textContent = `
     .popup-messages-container {

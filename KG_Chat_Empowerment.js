17cfd27b2f3757ffc18f8b0f6422720a8fdafcb8
Puncher <puncher@gmail.com>
2024-11-05 21:57:35 +0200

    snowflake currentColor changed to other color.

--- KG_Chat_Empowerment.js
+++ KG_Chat_Empowerment.js
@@ -7015,7 +7015,7 @@
        height="20" 
        viewBox="0 0 24 24" 
        fill="none" 
-       stroke="currentColor" 
+       stroke="lightsteelblue" 
        stroke-width="2" 
        stroke-linecap="round" 
        stroke-linejoin="round" 

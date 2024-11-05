dfcb8b7e100b2acdc6a48ae6483cd5c32572907e
Puncher <puncher@gmail.com>
2024-11-05 21:53:08 +0200

    snowflake svg 20 x 20

--- KG_Chat_Empowerment.js
+++ KG_Chat_Empowerment.js
@@ -7011,8 +7011,8 @@
   // Inline SVG source for the "snowflake" icon
   const snowflakeSVG = `
   <svg xmlns="http://www.w3.org/2000/svg" 
-       width="24" 
-       height="24" 
+       width="20" 
+       height="20" 
        viewBox="0 0 24 24" 
        fill="none" 
        stroke="currentColor" 

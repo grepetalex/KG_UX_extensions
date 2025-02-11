38ccbc8aa63c196f83f60e298a990201585ad684
Puncher <puncher@gmail.com>
2025-02-11 12:41:06 +0200

    Version 0.3

--- KG_Chat_Empowerment.js
+++ KG_Chat_Empowerment.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KG_Chat_Empowerment
 // @namespace    klavogonki
-// @version      0.2
+// @version      0.3
 // @description  Enhance the chat abilities
 // @author       Patcher
 // @match        *://klavogonki.ru/g*

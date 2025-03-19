2c9c606c95afc372341aaaaa06340e1f57fad057
Puncher <puncher@gmail.com>
2025-03-19 02:36:39 +0200

    Add meta tag to make chatlogs proper scale for mobile devices.

--- KG_Better_Chatlogs.js
+++ KG_Better_Chatlogs.js
@@ -234,6 +234,8 @@
     };
 
     const injectCustomStyles = () => {
+      document.head.insertAdjacentHTML('beforeend', '<meta name="viewport" content="width=device-width, initial-scale=1.0">');
+
       const style = document.createElement('style');
       style.textContent = `
         @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');

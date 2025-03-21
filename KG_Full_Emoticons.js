736807d0a4abf8b2ff55b532c155b1229bb289eb
Puncher <puncher@gmail.com>
2025-03-21 13:09:42 +0200

    Version up 1.5

--- KG_Full_Emoticons.js
+++ KG_Full_Emoticons.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KG_Full_Emoticons
 // @namespace    http://klavogonki.ru/
-// @version      1.4
+// @version      1.5
 // @description  Display a popup panel with every available emoticon on the site, remembering the last selected emoticon per category by name.
 // @match        *://klavogonki.ru/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=klavogonki.ru
